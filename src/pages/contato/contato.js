dcd5cf90dbe0ef4561ff12b4adbdd9c657342a7c
m0t44_ <94491523+M0T44@users.noreply.github.com>
2023-06-06 00:56:45 -0300

    att

--- src/pages/contato/contato.js
+++ src/pages/contato/contato.js
@@ -1,16 +1,11 @@
 import "../../css/style.contato.css"
-import Iframe from 'react-iframe'
 
 export default function Contato() {
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="paginaContato">
-
-                    <div className="iframeInsta">
-                        <Iframe className="insta" src="https://instagram.com/m0t44_?igshid=ZDc4ODBmNjlmNQ=="/>
-                    </div>
-
+                    pagina contato
                 </div>
             </div>
         </div>
